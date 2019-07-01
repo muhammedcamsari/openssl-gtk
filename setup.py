a73507340b6bd1311a7e8c0b7aaf738585dc7a44
Muhammed Çamsarı <muhammedcamsari@icloud.com>
2019-07-01 18:50:52 +0300

    pip düzenlemeleri
    Pip ile ilgili düzenlemeler yapıldı

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="openssl-gtk",
-    version="0.1.beta2",
+    version="1.0.2",
     author="Muhammed Çamsarı",
     license='MIT',
     author_email="Muhammedcamsari@icloud.com",
