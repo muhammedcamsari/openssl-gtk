797b1ea95f78962ab6bef5db562ca36ee0fd196e
Muhammed Çamsarı <muhammedcamsari@icloud.com>
2019-07-05 03:20:28 +0300

    Logo sorunu çözüldü
    Hakkımızda ve Sürüm notları penceresindeki logo gözükmeme sorununu çözer.

--- openssl_gtk/app_info.py
+++ openssl_gtk/app_info.py
@@ -4,6 +4,7 @@
 """
 Bu dosya uygulama hakkında bilgi tutan değişkenleri içerir.
 """
+import os
 
 __author__ = "Muhammed Çamsarı"
 __appname__ = "OpenSSL-GTK"
@@ -11,11 +12,14 @@ __definition__ = "Terminal kullanmadan Openssl işlemlerinizi gerçekleştirin."
 __legalnote__ = __appname__ + ' hiçbir garanti vermez.\nKullanım amacı tamamen kullanıcının sorumluluğundadır.\nDaha fazla bilgi için MIT lisansını inceleyiniz.'
 __copyright__ = "Copyright (c) 2019 " + __author__
 __license__ = "MIT"
-__version__ = "1.0.2"
-__status__ = "Beta"
+__version__ = "1.1"
+__status__ = "Alpha"
 __email__ = "muhammedcamsari@icloud.com"
 __pgp__ = 'F294 1D36 A8C8 101B EEB0  16A7 B260 DBA5 2DAA 962A'
-__logo__ = 'openssl_gtk/logo.png'
+
+__logo__ = os.path.dirname(__file__) + '/logo.png'
+
+print (__logo__)
 
 cipher_list = ['aes-128-cbc', 'aes-128-ecb', 'aes-192-cbc', 'aes-192-ecb', 'aes-256-cbc (Önerilen)', 'aes-256-ecb']
 digest_list = ['md5', 'md_gost94', 'ripemd160', 'sha1', 'sha224', 'sha256 (Önerilen)', 'sha384', 'sha512', 'streebog256', 'streebog512', 'whirlpool']
